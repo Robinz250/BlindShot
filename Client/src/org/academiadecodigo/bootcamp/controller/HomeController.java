029b884efe48f38794201f52b20f2500ca074e18
Pauwinho <p.sousa.design@gmail.com>
2017-06-26 20:52:31 +0100

    configure javafx, css and view

--- Client/src/org/academiadecodigo/bootcamp/controller/HomeController.java
+++ Client/src/org/academiadecodigo/bootcamp/controller/HomeController.java
@@ -1,7 +1,30 @@
 package org.academiadecodigo.bootcamp.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
+
 /**
  * Created by codecadet on 26/06/17.
  */
 public class HomeController {
+
+
+
+    @FXML
+    private GridPane ClientGrid;
+
+    @FXML
+    private Circle circle;
+
+    @FXML
+    void DragCircle(MouseEvent event) {
+        circle.setTranslateX(event.getSceneX() - circle.getRadius());
+        circle.setTranslateY(event.getSceneY() - circle.getRadius());
+        System.out.println("hey");
+    }
+
+
+
 }
--- Client/src/org/academiadecodigo/bootcamp/main.java
+++ Client/src/org/academiadecodigo/bootcamp/main.java
@@ -1,7 +1,9 @@
 package org.academiadecodigo.bootcamp;
 
 import javafx.application.Application;
+import javafx.scene.shape.Sphere;
 import javafx.stage.Stage;
+import org.academiadecodigo.bootcamp.utils.Navigation;
 
 /**
  * Created by codecadet on 26/06/17.
@@ -9,6 +11,13 @@ import javafx.stage.Stage;
 public class main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Navigation navigation = Navigation.getInstance();
+        navigation.setStage(primaryStage);
+
+        navigation.loadScreen("client");
+    }
+
+    public void init(){
 
     }
 }
